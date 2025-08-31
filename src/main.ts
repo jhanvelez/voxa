9bf742681e70a772ee20e33179fb68fb95657f3a
root <root@ip-172-31-18-198.us-east-2.compute.internal>
2025-08-31 22:35:06 +0000

    fix: Config for main https

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as express from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/test.sustentiatec.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/test.sustentiatec.com/fullchain.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+
+  app.enableCors({
+    //origin: 'http://localhost:3000',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   // Enable parsing of URL-encoded bodies (for Twilio webhook data)
   app.use(express.urlencoded({ extended: true }));
