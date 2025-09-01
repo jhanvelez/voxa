a31f762f1e0f40215b11204b0ca3c89b40a609f1
jhanvelez <jhanrvelez@gmail.com>
2025-09-01 05:53:21 +0000

    changes server

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as express from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
 import { WebSocketServer } from 'ws';
 import { VoiceGateway } from './voice/voice.gateway';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/test.sustentiatec.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/test.sustentiatec.com/fullchain.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+  app.enableCors({
+    //origin: 'http://localhost:3000',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   // Habilitar parsing de bodies (Twilio webhooks mandan x-www-form-urlencoded)
   app.use(express.urlencoded({ extended: true }));
@@ -17,26 +33,8 @@ async function bootstrap() {
   const wss = new WebSocketServer({ noServer: true });
 
   wss.on('connection', (ws, req) => {
-    console.log('Twilio WS conectado', req);
-
     const voiceGateway = app.get(VoiceGateway);
     voiceGateway.handleConnection(ws);
-
-    ws.on('message', (msg) => {
-      const data = JSON.parse(msg.toString());
-      console.log('Mensaje WS:', data);
-
-      // Ejemplo: echo test
-      if (data.event === 'media') {
-        ws.send(
-          JSON.stringify({
-            event: 'media',
-            streamSid: data.streamSid,
-            media: { payload: data.media.payload },
-          }),
-        );
-      }
-    });
 
     ws.on('close', () => {
       console.log('Twilio WS cerrado');
