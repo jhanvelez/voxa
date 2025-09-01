7000cdecb1f06f7b76ee69d35f7394d378b8c8ee
jhanvelez <jhanrvelez@gmail.com>
2025-08-31 19:09:51 -0500

    feat: Call URL to voice

--- src/twiml/twiml.controller.ts
+++ src/twiml/twiml.controller.ts
@@ -1,15 +1,27 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { twiml } from 'twilio';
+import * as twilio from 'twilio';
 
 @Controller()
 export class TwimlController {
   @Get('twiml')
   getTwiml(@Res() res: Response) {
-    const voiceResponse = new twiml.VoiceResponse();
-    voiceResponse.say('Hola, esta es tu propia respuesta personalizada.');
-    //res.type('text/xml');
+    const twiml = new twilio.twiml.VoiceResponse();
+
+    const gather = twiml.gather({
+      numDigits: 1,
+      action: 'https://test.sustentiatec.com:3001/menu',
+      method: 'POST',
+    });
+
+    gather.say(
+      { voice: 'alice', language: 'es-ES' },
+      'Bienvenido. Presione 1 para ventas. Presione 2 para soporte.',
+    );
+
+    twiml.say('No se recibió ninguna entrada. Adiós.');
+
     res.set('Content-Type', 'application/xml');
-    res.send(voiceResponse.toString());
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>${twiml.toString()}`);
   }
 }
